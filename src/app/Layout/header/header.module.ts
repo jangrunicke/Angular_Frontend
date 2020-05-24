420e846807ab3a047c1410c1a2e552a43f89f1aa
Jan Grunicke <jan.grunicke@web.de>
2020-05-24 22:37:37 +0200

    Header-Komponent

--- src/app/Layout/header/header.module.ts
+++ src/app/Layout/header/header.module.ts
@@ -1,10 +1,28 @@
+// import {
+//     MatButtonModule,
+//     MatToolbarModule,
+//     MatIconModule,
+//     MatListModule
+//     MatSidenavModule,
+// } from '@angular/material';
 import { HeaderComponent } from './header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
 
-    imports: [],
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+    ],
 })
 export class HeaderModule {}
